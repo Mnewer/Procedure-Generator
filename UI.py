a1877895d187f0b7b59b43346748634b9de8218e
Mnewer <jan.odvar@protonmail.com>
2020-10-29 19:48:29 +0100

    added splash screen

--- UI.py
+++ UI.py
@@ -13,43 +13,54 @@ def browseFiles():
     
     label_file_explorer.configure(text = str("File opened: " + filename))
 
-
-#root window:
-root = Tk()
-
-#root window attributes:
-root.title('Procedure Generator')
-root.geometry("700x500")
+#splash screen:
+splash_root = Tk()
+splash_root.geometry("500x600")
+splash_root_label = Label(splash_root, text = "Procedure Generator", font = "Helvetica",)
+splash_root_label.pack(pady = 90)
+splash_root.overrideredirect(True)
 logo = PhotoImage(file = "logo.png")
 logoimage = Label(image = logo)
-logoimage.place(x = 175, y = 150, width = 400, height = 249)
+logoimage.pack()
 
-label_file_explorer = Label(root,
-                            text = 'Find template:',
-                            width = 100,
-                            height = 4,
-                            fg = "blue")
+#root window:
+def main_window():
+    splash_root.destroy()
+    root = Tk()
+    root.title('Procedure Generator')
+    root.geometry("500x600")
+    # logo = PhotoImage(file = "logo.png")
+    # logoimage = Label(image = logo)
+    # logoimage.place(x = 175, y = 150, width = 400, height = 249)
 
-btn_explore = Button(root,
-                        text = "Browse templates",
-                        command = browseFiles)
+    label_file_explorer = Label(root,
+                                text = 'Find template:',
+                                width = 100,
+                                height = 4,
+                                fg = "blue")
 
-btn_exit = Button(root,
-                     text = "Exit",
-                     command = exit)
+    btn_explore = Button(root,
+                            text = "Browse templates",
+                            command = browseFiles)
+
+    btn_exit = Button(root,
+                        text = "Exit",
+                        command = exit)
 
 
 #Placing of widgets:
-label_file_explorer.grid(column = 1, row = 1)
+    label_file_explorer.grid(column = 1, row = 1)
 
-btn_explore.grid(column = 1, row = 2)
+    btn_explore.grid(column = 1, row = 2)
 
-btn_exit.grid(column = 1, row = 3)
-
-
+    btn_exit.grid(column = 1, row = 3)
 
 
 
+#Splash screen timer:
+splash_root.after(3000, main_window)
 
 
-root.mainloop()
+
+
+mainloop()
